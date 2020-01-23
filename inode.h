78607d21a131929e2204c7168371eb9a079bb648
hding1 <hding@umail.ucsb.edu>
2020-01-23 14:41:33 -0800

    Add some comments

--- inode.h
+++ inode.h
@@ -1,9 +1,12 @@
-
-#define DIRECT_BLKS_NUM 
+#define INODE_MAX_NUM 50000
+#define DIRECT_BLKS_NUM 10
 
 struct inode{
      //owner info
-
+     int permission;
+     int file_name;
+     int file_type;
+     int path;
      //size
      int size;
      //link
@@ -17,7 +20,10 @@ struct inode{
      int double_ind;
      int triple_ind;   
 }
+extern inode ilist[INODE_MAX_NUM];
 
-inode_list_init();
-int inode_read(inode* node);
-int inode_write(inode* node);
+inode_list_init(); //initlaize inode list and write all inodes to the disk
+int inode_read(inode* node); //read inode from the disk with index 
+int inode_write(inode* node); // write inode to the disk
+int inode_free(int index); // free inode from inode list at index i
+inode* inode_allocate(); // allocate an inode
