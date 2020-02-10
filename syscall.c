9cac188f7819093610ec6ab68f4f6a54575a790b
hding1 <dhn.peter1@gmail.com>
2020-02-10 15:06:12 -0800

    update syscall.c for inode new signature

--- syscall.c
+++ syscall.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dir.h>
+#include <sys/stat.h>
 
 // get parent name, filename and parent path from path
 int get_parent(const char *path, char * parent, char * filename, char ** parentPath){
@@ -34,16 +35,16 @@ char * get_dir(char *path){
 	return copy;
 }
 
-// read in an inode parent directory and target
+// read in an inum parent directory and target
 // return inode number if found, if not return -1
-int find_inode_index(inode * parent, char * target){
+int find_inode_index(int inum, char * target){
 	// start number = 2, since 0 and 1 are for . and ..
 	unsigned int start = 2;
 	unsigned int offset = start*DIRENT_SIZE;
 	char * tempbuf = (char*)malloc(DIRENT_SIZE);
-	while(offset <= parent->size){
+	while(offset <= read_inode_size(inum)){
 		// read one entry at an time
-		if(read_file(&parent, &tempbuf, DIRENT_SIZE, offset)==-1){
+		if(read_file(inum, &tempbuf, DIRENT_SIZE, offset)==-1){
 			perror("Error Unable to Read");
 			return -1;
 		}
@@ -51,18 +52,20 @@ int find_inode_index(inode * parent, char * target){
 		dirent * mydirent = (dirent *) tempbuf;
 		// compare the dirent with target
 		if(strcmp(mydirent->name,target)==0){
+			free(tempbuf);
 			return mydirent->inum;
 		}
 		start++;
 		offset = start*DIRENT_SIZE;
 	}
 	// no target found
+	free(tempbuf);
 	return -1;
 }
 
-// Use an path to find the inode of the last element
+// Given an path to find the inode of the last element
 // return -1 if not found, inumber if found 
-int find_inode(const char *path, inode ** node){	
+int find_inode(const char *path){	
 	// make a copy of path
 	char* pathCopy = (char*)malloc(strlen(path) + 1);
 	pathCopy = path;
@@ -77,27 +80,18 @@ int find_inode(const char *path, inode ** node){
 	}
 	// get root
 	int myInum = get_root_inum();
-	inode * myInode = malloc(sizeof(*myInode));
-	if(inode_read(&myInode, myInum)==-1){
-		perror("Error Getting Root Inode");
-		return -1;
-	}
+	
 	// loop throught the path
 	while(strlen(pathCopy)!=0){
 		char* dir = get_dir(pathCopy);
-		myInum = find_inode_index(myInode,dir);
+		myInum = find_inode_index(myInum,dir);
 		if(myInum == -1){
 			printf("%s\n",path);
 			perror("Error Cannot Find Directory");
 			return -1;
 		}
-		if(inode_read(&myInode, myInum)==-1){
-			perror("Error Getting Inode");
-			return -1;
-		}
-		path = path+strlen(dir)+1;
+		pathCopy = pathCopy+strlen(dir)+1;
 	}
-	*node = myInode;
 	free(pathCopy);
 	return myInum;
 }
@@ -105,23 +99,22 @@ int find_inode(const char *path, inode ** node){
 // Create a directory
 int pcd_mkdir(const char *path, mode_t mode)
 {
-	inode* newNode;
-	int myInum = inode_allocate(&newNode);
+	int myInum = inode_allocate();
 
 	if(myInum == -1 ){
 		perror("Error Inode Allocation Failed");
 		return -1;
 	}
 	//set inode type
-	newNode->mode = mode;
+	write_inode_mode(myInum, mode);
 
 	//write current node id into the parent
-	inode* parentNode;
 	char parentName[MAX_FILE_NAME];
 	char fileName[MAX_FILE_NAME];
 	char * parentPath;
 	get_parent(path,parentName,fileName,&parentPath);
-	int parentInum = find_inode(path, &parentNode);
+	// find inode of the parent
+	int parentInum = find_inode(parentPath);
 	if(parentInum==-1){
 		perror("Error Cannot Find Directory Inode");
 		return -1;
@@ -132,60 +125,93 @@ int pcd_mkdir(const char *path, mode_t mode)
 	dir.name = parentName;
 	char* buf = (char*)malloc(DIRENT_SIZE);
 	memcpy(buf,&dir,DIRENT_SIZE);
-	if(write_file(&parentNode, buf, DIRENT_SIZE, parentNode->size)==-1){
+	if(write_file(parentInum, &buf, DIRENT_SIZE, read_inode_size(parentInum))==-1){
 		perror("Error Writing to File");
-		return -1;
-	}
-	if(inode_write(&parentNode,parentInum)==-1){
-		perror("Error Writing Inode");
 		return -1;
 	}
 	//write . and .. to the inode
 	dir = {myInum,'d',"."};
 	char* buf = (char*)malloc(DIRENT_SIZE);
 	memcpy(buf,&dir,DIRENT_SIZE);
-	if(write_file(&newNode, buf, DIRENT_SIZE, newNode->size)==-1){
+	if(write_file(myInum, &buf, DIRENT_SIZE,read_inode_size(myInum))==-1){
 		perror("Error Writing to File");
 		return -1;
 	}
 	dir = {parentInum,'d',".."};
 	char* buf = (char*)malloc(DIRENT_SIZE);
 	memcpy(buf,&dir,DIRENT_SIZE);
-	if(write_file(&newNode, buf, DIRENT_SIZE, newNode->size)==-1){
+	if(write_file(myInum, &buf, DIRENT_SIZE, read_inode_size(myInum))==-1){
 		perror("Error Writing to File");
-		return -1;
-	}
-	if(inode_write(&newNode,myInum)==-1){
-		perror("Error Writing Inode");
 		return -1;
 	}
 	return 0;
 }
 
+
+// read in an inum parernt directory and target
+// delete the dirent under parent
+// return 0 if success, if not return -1
+int delete_dirent(int inum, char * target){
+	// start number = 2, since 0 and 1 are for . and ..
+	unsigned int start = 2;
+	unsigned int offset = start*DIRENT_SIZE;
+	char * tempbuf = (char*)malloc(DIRENT_SIZE);
+	while(offset <= read_inode_size(inum)){
+		// read one entry at an time
+		if(read_file(inum, &tempbuf, DIRENT_SIZE, offset)==-1){
+			perror("Error Unable to Read");
+			return -1;
+		}
+		// cast the buffer to dirent
+		dirent * mydirent = (dirent *) tempbuf;
+		// compare the dirent with target
+		if(strcmp(mydirent->name,target)==0){
+			//if found, rename the entry to empty ""
+			strcpy(mydirent->name, "");
+			mydirent->inum = -1;
+			char* buf = (char*)malloc(DIRENT_SIZE);
+			memcpy(buf,mydirent,DIRENT_SIZE);
+			if(write_file(myInum, &buf, DIRENT_SIZE,offset)==-1){
+				perror("Error Writing to File");
+				return -1;
+			}
+			free(tempbuf);
+			return 0;
+		}
+		start++;
+		offset = start*DIRENT_SIZE;
+	}
+	free(tempbuf);
+	// no target found
+	return -1;
+}
+
 // Remove a file
 int pcd_unlink(const char *path)
 {
-	inode* myNode;
-	int myInum = find_inode(path, &myNode);
+	int myInum = find_inode(path);
 	if(myInum==-1){
 		perror("Error Cannot Find Inode");
 		return -1;
 	}
-	// reduce link count
-	myNode->link_count--;
-	if(myNode->link_count==0){
-		//remove file
-		if(inode_free(myInum)==-1){
-			perror("Error Freeing Inode");
-			return -1;
-		}
-	}else{
-		//write to inode
-		if(inode_write(&myNode,myInum)==-1){
-			perror("Error Writing Inode");
-			return -1;
-		}
+
+	// remove from parent
+	// write current node id into the parent
+	char parentName[MAX_FILE_NAME];
+	char fileName[MAX_FILE_NAME];
+	char * parentPath;
+	get_parent(path,parentName,fileName,&parentPath);
+	// find inode of the parent
+	int parentInum = find_inode(parentPath);
+	delete_dirent(parentInum, filename);
+
+	if(parentInum==-1){
+		perror("Error Cannot Find Directory Inode");
+		return -1;
 	}
+
+	// reduce link count
+	reduce_inode_link(myInum);
 	return 0;
 }
 
@@ -205,14 +231,13 @@ char FileType (mode_t m) {
 // Create a node (file, device special, or named pipe)
 int pcd_mknod(const char *path, mode_t mode, dev_t rdev)
 {
-	inode* newNode;
-	int myInum = inode_allocate(&newNode);
+	int myInum = inode_allocate();
 	if(myInum == -1 ){
 		perror("Error Inode Allocation Failed");
 		return -1;
 	}
 	//set inode type
-	newNode->mode = mode;
+	write_inode_mode(myInum, mode);
 	char fileType = FileType(mode);
 	//write current node id into the parent directory
 	inode* parentNode;
@@ -220,7 +245,7 @@ int pcd_mknod(const char *path, mode_t mode, dev_t rdev)
 	char fileName[MAX_FILE_NAME];
 	char * parentPath;
 	get_parent(path,parentName,fileName,&parentPath);
-	int parentInum = find_inode(path, &parentNode);
+	int parentInum = find_inode(path);
 	if(parentInum==-1){
 		perror("Error Cannot Find Directory Inode");
 		return -1;
@@ -228,20 +253,11 @@ int pcd_mknod(const char *path, mode_t mode, dev_t rdev)
 	dirent dir;
 	dir.inum = myInum;
 	dir.file_type = fileType;
-	dir.name = parentName;
+	dir.name = fileName;
 	char* buf = (char*)malloc(DIRENT_SIZE);
 	memcpy(buf,&dir,DIRENT_SIZE);
-	if(write_file(&parentNode, buf, DIRENT_SIZE, parentNode->size)==-1){
+	if(write_file(parentInum, &buf, DIRENT_SIZE, read_inode_size(parentInum))==-1){
 		perror("Error Writing to File");
-		return -1;
-	}
-	if(inode_write(&parentNode,parentInum)==-1){
-		perror("Error Writing Inode");
-		return -1;
-	}
-	//write itself to the inode
-	if(inode_write(&newNode,myInum)==-1){
-		perror("Error Writing Inode");
 		return -1;
 	}
 	return 0;
@@ -293,14 +309,13 @@ int pcd_read(const char *path, char *buf, size_t size, off_t offset,
 		    struct fuse_file_info *fi)
 {
 	//path get inode and inum
-	inode* myNode;
-	int myInum = find_inode(path, &myNode);
+	int myInum = find_inode(path);
 	if(myInum==-1){
 		perror("Error Cannot Find Inode");
 		return -1;
 	}
 	//read to buf
-	if(read_file(&myNode, &buf, size, offset)==-1){
+	if(read_file(myInum, &buf, size, offset)==-1){
 		perror("Error Reading File");
 		return -1;
 	}
@@ -312,14 +327,13 @@ int pcd_write(const char *path, const char *buf, size_t size,
 		     off_t offset, struct fuse_file_info *fi)
 {
 	//path get inode and inum
-	inode* myNode;
-	int myInum = find_inode(path, &myNode);
+	int myInum = find_inode(path);
 	if(myInum==-1){
 		perror("Error Cannot Find Inode");
 		return -1;
 	}
 	//read to buf
-	if(write_file(&myNode, buf, size, offset)==-1){
+	if(write_file(myInum, &buf, size, offset)==-1){
 		perror("Error Writing File");
 		return -1;
 	}
