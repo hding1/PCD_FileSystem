8703948f9987857bf8515b06fb76bb9244c67ed2
Fuheng Zhao <fuheng_zhao@umail.ucsb.edu>
2020-02-29 22:46:30 -0800

    change memcpy to file read/write

--- db.c
+++ db.c
@@ -19,17 +19,22 @@ unsigned int db_allocate(){
 	}
 	void* buffer = malloc(sizeof(char) * DB_SIZE);
 	disk_read(buffer,bid);
+
+	if(bid>super->NUM_BLOCK){
+		printf("bid %u too large in allocation\n",bid);
+                bid=-1;
+		super->NUM_FREE_BLOCK =0;
+		sb_write(super);
+		free(super);
+		free(buffer);
+		return bid;
+	}
 	unsigned int* new_free_id = malloc(sizeof(unsigned int));
 	memcpy(new_free_id, buffer, sizeof(unsigned int));
 
-	super->FREE_LIST = *new_free_id;
-	
+	super->FREE_LIST = *new_free_id;	
 	sb_write(super);
 
-	if(bid > super->NUM_BLOCK){
-		printf("bid %u too large in allocation\n",bid);
-		bid=-1;
-	}
 	free(super);
 	free(buffer);
 	return bid;
@@ -101,18 +106,6 @@ int db_write(void* in, unsigned int block_id){
 	disk_write(in, block_id);
 	return 0; // Return 0 on success
 }
-
-/*
-void disk_read(void* out, unsigned int block_id){
-	void* Disk_Buffer = (void*)((char*)add_0 + block_id*4096);
-	memcpy(out, Disk_Buffer, DB_SIZE);
-}
-void disk_write(void* in, unsigned int block_id){
-	void* Disk_Buffer = (void*)((char*)add_0 + block_id*4096);
-	memcpy(Disk_Buffer, in, DB_SIZE);
-}
-*/
-
 void db_init(){
 	sb* super = sb_read();
 	unsigned int START_DATA_BLOCK = super->START_DATA_BLOCK;
