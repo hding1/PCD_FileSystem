9a14fab18e494347137f0173a1d6b883e3871fe7
Zihao Zhang <zihaozhang@ucsb.edu>
2020-03-01 17:38:20 -0800

    inode finalized

--- inode.h
+++ inode.h
@@ -47,7 +47,7 @@ typedef struct inode{
      time_t last_modified;
 
      // Data blocks
-     unsigned int direct_blo[DIR_ID_NUM];
+     unsigned int direct_blo[12];
      unsigned int single_ind;
      unsigned int double_ind;
      unsigned int triple_ind;
