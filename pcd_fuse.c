5ecb6cddc67774df1f21c962ce3b2f05dda1294f
Karl Wang <kovl.void@gmail.com>
2020-03-05 21:03:45 -0800

    initialize and final sync

--- pcd_fuse.c
+++ pcd_fuse.c
@@ -75,16 +75,15 @@ static struct fuse_operations pcd_oper = {
  * different values on the command line.
  */
 static struct options {
-	const char *filename;
-	const char *contents;
+	const char *device;
 	int show_help;
 } options;
 
 #define OPTION(t, p)                           \
     { t, offsetof(struct options, p), 1 }
 static const struct fuse_opt option_spec[] = {
-	OPTION("--name=%s", filename),
-	OPTION("--contents=%s", contents),
+	OPTION("-d %s", device),
+	OPTION("--device=%s", device),
 	OPTION("-h", show_help),
 	OPTION("--help", show_help),
 	FUSE_OPT_END
@@ -94,10 +93,7 @@ static void show_help(const char *progname)
 {
 	printf("usage: %s [options] <mountpoint>\n\n", progname);
 	printf("File-system specific options:\n"
-	       "    --name=<s>          Name of the \"hello\" file\n"
-	       "                        (default: \"hello\")\n"
-	       "    --contents=<s>      Contents \"hello\" file\n"
-	       "                        (default \"Hello, World!\\n\")\n"
+	       "    --device=<s>          Path of the block device\n"
 	       "\n");
 }
 
@@ -109,8 +105,7 @@ int main(int argc, char *argv[])
 	/* Set defaults -- we have to use strdup so that
 	   fuse_opt_parse can free the defaults if other
 	   values are specified */
-	options.filename = strdup("pcd");
-	options.contents = strdup("pcd World!\n");
+	options.device = strdup("undefined");
 
 	/* Parse options */
 	if (fuse_opt_parse(&args, &options, option_spec, NULL) == -1)
@@ -121,13 +116,21 @@ int main(int argc, char *argv[])
 	   additional help (by adding `--help` to the options again)
 	   without usage: line (by setting argv[0] to the empty
 	   string) */
-	if (options.show_help) {
+	if (options.show_help || strcmp(options.device, "undefined") == 0) {
 		show_help(argv[0]);
 		assert(fuse_opt_add_arg(&args, "--help") == 0);
 		args.argv[0][0] = '\0';
 	}
+	initialize(options.device);
 
 	ret = fuse_main(args.argc, args.argv, &pcd_oper, NULL);
 	fuse_opt_free_args(&args);
+
+	printf("syncing...");
+	sync();
+
+	printf("freeing in memory stuff...");
+	freefs();
+
 	return ret;
 }
